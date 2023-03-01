d858c93de1b19f9aef544d03dba9032ee9f0a919
MSI\haoyi <haoyin_chong@hotmail.com>
2023-03-01 16:58:59 +0800

    First commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,57 @@
+import { AnimatePresence } from 'framer-motion'
+import { useState } from 'react'
+import {LayoutGroup} from 'framer-motion'
+import './App.css'
+import TopNotchCard from './Components/MainMenu/TopNotchCard'
+import EmploymentCard from './Components/MainMenu/EmploymentCard'
+import OutsourcedPayrollCard from './Components/MainMenu/OutsourcedPayrollCard'
+import SeekingOutCard from './Components/MainMenu/SeekingOutCard'
+import {motion} from 'framer-motion'
+import HumanResourcesCard from './Components/MainMenu/HumanResourcesCard'
+import JoinusCard from './Components/MainMenu/JoinusCard'
+import OurstoryCard from './Components/MainMenu/OurstoryCard'
+import CareertipsCard from './Components/MainMenu/CareertipsCard'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [clickedId,setClickedId] = useState()
+  console.log(clickedId)
+  return (
+    <div className="">
+      <motion.div className="w-auto">
+      <LayoutGroup>
+
+      <motion.div className='flex items-end'>
+        
+        <motion.div>
+        <TopNotchCard/>
+        <SeekingOutCard />
+        </motion.div>
+
+        <motion.div>
+        <EmploymentCard />
+        <HumanResourcesCard />
+        </motion.div>
+        
+        <motion.div>
+        <OutsourcedPayrollCard />
+        <JoinusCard />
+        </motion.div>
+      
+        <motion.div>
+        <CareertipsCard />
+        <OurstoryCard />
+        </motion.div>
+      </motion.div>
+      <motion.div className='flex'>
+      
+      
+      </motion.div>
+      </LayoutGroup>
+      </motion.div>
+
+    </div>
+  )
+}
+
+export default App
---
+++ vite-project/src/Components/MainMenu/SeekingOutCard.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import {motion, AnimatePresence} from 'framer-motion'
+import OutsideClickHandler from 'react-outside-click-handler'
+import { BsArrowRightCircle } from 'react-icons/bs'
+import {IconContext} from 'react-icons'
+
+const SeekingOutCard = () => {
+  
+    const [isOpen,setIsOpen] = useState(false);
+
+  return (
+    <OutsideClickHandler onOutsideClick={()=>setIsOpen(false)}>
+      <motion.div whileHover={{scale:1.05}}
+      transition={{type:"spring"}}
+    //    transition={{layout:{duration:1,type:"spring"}}} 
+    // transition={{ type: "spring", stiffness: 100, damping: 10}}
+    layout
+      onClick={()=>setIsOpen(true)}
+      className={`text-${isOpen? 'left w-auto':'center w-60 h-80'} bg-stone-200 py-8 px-6 mx-2 my-4 hover:bg-blue-900 hover:text-slate-200`}
+      style={{borderRadius:'1rem'}}
+    >
+        <motion.div className={`flex items-end h-${isOpen ? '12':'48'} h- mr-16`}>
+        <motion.h2 layout="position" className='text-2xl font-light text-left'>SEEKING OUT?</motion.h2> 
+        </motion.div>
+
+        <motion.div className={`pt-4 ${isOpen&& 'hidden'}`}>
+            <IconContext.Provider value={{size:"3em"}}>
+            <BsArrowRightCircle />
+            </IconContext.Provider>            
+        </motion.div>
+        {isOpen ? (
+            <motion.div 
+            initial={{opacity:0}}
+            animate={{opacity:1}}
+            transition={{duration:1}}
+            className=' w-96'>
+            <p className='pt-4 leading-6'>Exmple, Hello welcome to topnotch~ </p>
+            <p className='pt-4 leading-6 inline'>Collapsible components put long sections of information under a block, which enables users to expand and access its features. The uniqueness of being a developer is implementing anything to be built in a particular way. In our case, we want animated collapsible components. Though libraries like react-collapse could help, you may want to build these things yourself.</p>
+          </motion.div>
+        ):null}
+        
+    </motion.div>
+    </OutsideClickHandler>
+  )
+}
+
+export default SeekingOutCard
